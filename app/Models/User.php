96b2fec8840b185e5b22cae7d9ab99883d548ca4
Thibault CHAZOTTES <thib.chazottes@gmail.com>
2020-10-05 10:44:47 +0200

    :construction:

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function commentsOnPost()
+    {
+        return $this->hasManyThrough(Comment::class, Post::class);
+    }
 }
